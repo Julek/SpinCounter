67734e7d65087bc93f4fea85cae6f8442cfda197
Julian Sutherland <julian.sutherland10@imperial.ac.uk>
2015-01-24 01:40:00 +0000

    0.0.1 full

--- Data/NonBlocking/LockFree/SpinCounter.hs
+++ Data/NonBlocking/LockFree/SpinCounter.hs
@@ -1,3 +1,13 @@
+
+{-|
+Module      : SpinCounter
+Description : Implementation of a lock-free Spin Counter.
+License     : BSD3
+Maintainer  : Julian Sutherland (julian.sutherland10@imperial.ac.uk)
+
+An implementation of a lock-free spin counter. Works with any monad that has atomically modificable references.
+-}
+
 module Data.NonBlocking.LockFree.SpinCounter(SpinCounter(), SpinCounterIO, SpinCounterSTM, newSpinCounter, incSpinCounter, readSpinCounter) where
 
 import Control.Concurrent.STM (STM())
@@ -6,16 +16,21 @@ import Control.Monad.Loops(whileM_)
 import Control.Monad.Ref(MonadAtomicRef, newRef, readRef, writeRef, atomicModifyRef)
 import Data.IORef(IORef)
 
+-- |SpinCounter inside the IO Monad.
 type SpinCounterIO = SpinCounter IORef
+-- |SpinCounter inside the STM Monad.
 type SpinCounterSTM = SpinCounter TVar
 
+-- |A lock-free concurrent Spin counter usable in any monad, m, that is paired with a reference type, r, by an instance of 'MonadAtomicRef'. Can use Specializations 'SpinCounterIO' and 'SpinCounterSTM'
 data SpinCounter r = SpinCounter (r Integer)
 
-{-# SPECIALIZE newSpinCounter :: IO (SpinCounterIO)   #-}
-{-# SPECIALIZE newSpinCounter :: STM (SpinCounterSTM) #-}
-newSpinCounter :: (MonadAtomicRef r m) => m (SpinCounter r)
-newSpinCounter = newRef 0 >>= return . SpinCounter
+-- |Creates a new instance of the 'SpinCounter' data type initialized to value of the input to the function, an instance of the class 'Integral'.
+{-# SPECIALIZE newSpinCounter :: (Integral a) => a -> IO (SpinCounterIO)   #-}
+{-# SPECIALIZE newSpinCounter :: (Integral a) => a -> STM (SpinCounterSTM) #-}
+newSpinCounter :: (MonadAtomicRef r m, Integral a) => a -> m (SpinCounter r)
+newSpinCounter n = newRef (toInteger n) >>= return . SpinCounter
 
+-- |Increments an instance of the 'SpinCounter' data type by one in a lock-free manner.
 {-# SPECIALIZE incSpinCounter :: SpinCounterIO -> IO ()   #-}
 {-# SPECIALIZE incSpinCounter :: SpinCounterSTM -> STM () #-}
 incSpinCounter :: (MonadAtomicRef r m) => SpinCounter r -> m ()
@@ -25,6 +40,7 @@ incSpinCounter (SpinCounter ref) = do
     v <- readRef ref
     cas ref v (v+1) >>= writeRef b
 
+-- |Reads the value of an instance of the 'SpinCounter' data type in a lock-free manner.
 {-# SPECIALIZE readSpinCounter :: (Num a) => SpinCounterIO -> IO a   #-}
 {-# SPECIALIZE readSpinCounter :: (Num a) => SpinCounterSTM -> STM a #-}
 readSpinCounter :: (MonadAtomicRef r m, Num a) => SpinCounter r -> m a
